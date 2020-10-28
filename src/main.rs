710d62a8a464f2cd955583d227fcc85d7e724967
yerowyn <yerowyn@gmail.com>
2020-10-28 15:53:21 +0600

    chapter 3: logging

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use env_logger;
 use std::fs;
 
 fn main() {
+	env_logger::init();
+
 	let dataset = "datasets/census.csv";
-	println!("Reading dataset from {}", dataset);
+	log::info!("Reading dataset from {}", dataset);
 	let file = fs::File::open(dataset).expect("Cannot read dataset");
 	let mut reader = csv::Reader::from_reader(file);
 
-	println!("Parsing CSV records");
+	log::info!("Parsing CSV records");
 	for record in reader.records() {
 		let record = record.expect("Invalid record");
-		println!("{:?}", record)
+		log::trace!("{:?}", record)
 	}
 }
